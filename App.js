f62a5ea94f8c0be4e8fb5d164fcce691352bfd5c
= <mukul02015@gmail.com>
2020-11-12 14:59:05 +1100

    moves app code to login

--- App.js
+++ App.js
@@ -8,108 +8,11 @@ import AssetExample from './components/AssetExample';
 
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
+import Login from './src/scenes/Login';
 
 export default function App() {
 
-  // Cognito Pool definition instantiation
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const poolData = {
-    UserPoolId: 'us-east-1_riQAYckmR',
-    ClientId: 't7i2mg2lp5i28nai88ldk635q',
-  }
-  const userPool = new CognitoUserPool(poolData);
-
-  // click sign in action
-  const onSignIn = event => {
-    event.preventDefault();
-    console.log('Signing in..');
-    userPool.signIn()
-  }
-  // click sign up action
-  const onSignUp = event => {
-    event.preventDefault();
-    console.log('Signing up..');
-    userPool.signUp(email, password, [], null, (err, data) => {
-      if (err) console.error(err);
-      console.log(data);
-    });
-  }
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Image source={require('./assets/logo.png')} style={styles.logo} />
-      <Text style={styles.logoText}>
-        Lets Find &#8226; Lets Talk &#8226; Lets Meet
-      </Text>
-      <View style={styles.loginSection}>
-        <TextInput
-          style={styles.loginInput}
-          selectionColor='white'
-          textContentType="username"
-          placeholder="Username/ Phone number/ Email" 
-          placeholderTextColor="grey"
-          value={email}
-          onChange={event=>setEmail(event.target.value)}
-        />
-        <TextInput
-          color="white"
-          style={styles.loginInput}
-          selectionColor='white'
-          secureTextEntry="true"
-          textContentType="password"
-          placeholder="Password"
-          placeholderTextColor="grey"
-          value={password}
-          onChange={event=>setPassword(event.target.value)}
-        />
-        <Button style={styles.loginButton} color="pink" title="Sign in" onPress={onSignIn} />
-        <Text style={styles.loginText}>Don't have an account?</Text>
-        <Button style={styles.loginButton} color="pink" title="Sign up" onPress={onSignUp} />
-      </View>
-    </SafeAreaView>
+  return(
+    <Login/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    flex: 1,
-    paddingTop: Constants.statusBarHeight,
-    padding: 8,
-    
-  },
-  logo: {
-    marginTop: 100,
-    height: 100,
-    width: 100,
-    alignSelf: 'center',
-  },
-  logoText: {
-    marginTop: 10,
-    color: 'pink',
-    alignSelf: 'center',
-    fontSize: 16,
-  },
-  loginSection: {
-    marginTop: 80,
-  },
-  loginText: {
-    color: 'black',
-    textAlign: 'center',
-  },
-  loginInput: {
-    height: 50,
-    padding: 8,
-    color: 'white',
-    margin: 10,
-    fontSize: 16,
-    backgroundColor: '#326da8',
-    textAlign: 'bottom',
-    borderRadius: 20,
-  },
-  loginButton: {
-    padding: 8,
-    fontWeight: 'bold',
-  },
-});
---
+++ src/styles/colors.js
@@ -0,0 +1,8 @@
+export default{
+    primary:"white",    
+    inputBackground: "#326da8",
+    buttonColor: 'pink',
+    displayText: 'black',
+    inputText: 'white',
+    logoText: 'pink',
+};
